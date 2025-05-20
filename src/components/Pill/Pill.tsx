76473577a66401b85d5092165c6eb6382cc60997
andrem123 <A01664892@tec.mx>
2025-05-19 18:18:49 -0600

    Borré pill componente

--- src/components/Pill/Pill.tsx
+++
@@ -1,9 +0,0 @@
-import { Badge } from "@/components/ui/badge";
-
-type PillProps = {
-  text: string;
-};
-
-export default function Pill({ text }: PillProps) {
-  return <Badge className="px-3 py-0.5 text-xs">{text}</Badge>;
-}
